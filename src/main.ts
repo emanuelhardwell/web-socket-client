5caed559bdabd063dc1f6fa68b9df27448b98b51
emanuelhardwell <emanuelnoe16@gmail.com>
2025-02-03 20:06:05 -0600

    feat: On event clients_connected

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Web Sockets - Client!</h1>
     <p id="server-status">Offline.</p>
+    <ul id="clients_list">
+    </ul>
   </div>
 `;
 
